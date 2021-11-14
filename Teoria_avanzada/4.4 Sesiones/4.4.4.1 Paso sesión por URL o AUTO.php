da969eaf60c4f412d886cf941b6a1b9abb548831
Santi Stars <santi_r6@hotmail.es>
2021-11-14 19:21:49 +0100

    Teoria PHP

--- Teoria_avanzada/4.4 Sesiones/4.4.4.1 Paso sesión por URL o AUTO.php
+++ Teoria_avanzada/4.4 Sesiones/4.4.4.1 Paso sesión por URL o AUTO.php
@@ -1,6 +1,8 @@
 <?php
+// Propagación automática---------------------------------
 ini_set("session.use_only_cookies","0");    // Esto se pone al principio del archivo para que no dé ERROR
-ini_set("session.use_trans_sid","1");       // Esto se pone al principio del archivo para que no dé ERROR
+ini_set("session.use_trans_sid","1");       //  "   "   "   "
+// --------------------------------------------------------
 session_start();
 $_SESSION["nombre"] = "Silvia Ruiz";
 $_SESSION["edad"] = 32;
@@ -9,9 +11,11 @@ if (!isset($_SESSION['contador'])) {
 } else {
     $_SESSION['contador']++;
 }
-// Propagación por URL
+// Propagación por URL------------------------------------
 // $id_sesion = SID;
 // echo "<a href=\"4.4.4.1 Ver sesion pasada por URL o AUTO.php?$id_sesion\">Pasar variables</ a>";
+// --------------------------------------------------------
 
-// Propagación automática
-echo ";
+// Propagación automática---------------------------------
+echo "<a href=\"4.4.4.1 Ver sesion pasada por URL o AUTO.php\">Pasar variables</ a>";
+// --------------------------------------------------------
